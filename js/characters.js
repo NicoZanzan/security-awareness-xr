2b0708b6745b88b18cefb364ff56e4574fd30023
Nico Cmt <comotti.n@gmail.com>
2025-06-23 13:02:04 +0200

    comment out js specifics

--- js/characters.js
+++ js/characters.js
@@ -1,142 +1,142 @@
-// characters.js - Character management and positioning
+// // characters.js - Character management and positioning
 
-// import * as THREE from 'three';
+// // import * as THREE from 'three';
 
-// Character placement settings
-const CHARACTER_SETTINGS = {
-  wendy: {
-    scale: 1.0,
-    position: { x: 0, y: 0, z: -2 }, // In front of user
-    animations: {
-      idle: 'Idle',
-      talking: 'Talking',
-      greeting: 'Greeting'
-    }
-  },
-  mendy: {
-    scale: 1.0,
-    position: { x: 0, y: 0, z: 2 }, // Behind user
-    animations: {
-      idle: 'Idle',
-      talking: 'Talking',
-      greeting: 'Greeting'
-    }
-  }
-};
+// // Character placement settings
+// const CHARACTER_SETTINGS = {
+//   wendy: {
+//     scale: 1.0,
+//     position: { x: 0, y: 0, z: -2 }, // In front of user
+//     animations: {
+//       idle: 'Idle',
+//       talking: 'Talking',
+//       greeting: 'Greeting'
+//     }
+//   },
+//   mendy: {
+//     scale: 1.0,
+//     position: { x: 0, y: 0, z: 2 }, // Behind user
+//     animations: {
+//       idle: 'Idle',
+//       talking: 'Talking',
+//       greeting: 'Greeting'
+//     }
+//   }
+// };
 
-// Load and prepare character models
-export async function loadCharacters(modelAssets, mixers) {
-  const characters = {};
+// // Load and prepare character models
+// export async function loadCharacters(modelAssets, mixers) {
+//   const characters = {};
   
-  // Process wendy character
-  if (modelAssets.wendy) {
-    characters.wendy = await prepareCharacter(
-      modelAssets.wendy, 
-      CHARACTER_SETTINGS.wendy,
-      mixers
-    );
-  }
+//   // Process wendy character
+//   if (modelAssets.wendy) {
+//     characters.wendy = await prepareCharacter(
+//       modelAssets.wendy, 
+//       CHARACTER_SETTINGS.wendy,
+//       mixers
+//     );
+//   }
   
-  // Process mendy character
-  if (modelAssets.mendy) {
-    characters.mendy = await prepareCharacter(
-      modelAssets.mendy, 
-      CHARACTER_SETTINGS.mendy,
-      mixers
-    );
-  }
+//   // Process mendy character
+//   if (modelAssets.mendy) {
+//     characters.mendy = await prepareCharacter(
+//       modelAssets.mendy, 
+//       CHARACTER_SETTINGS.mendy,
+//       mixers
+//     );
+//   }
   
-  return characters;
-}
+//   return characters;
+// }
 
-// Prepare a character model and its animations
-async function prepareCharacter(gltfAsset, settings, mixers) {
-  // Clone the model scene
-  const model = gltfAsset.scene.clone();
+// // Prepare a character model and its animations
+// async function prepareCharacter(gltfAsset, settings, mixers) {
+//   // Clone the model scene
+//   const model = gltfAsset.scene.clone();
   
-  // Apply scale
-  model.scale.setScalar(settings.scale);
+//   // Apply scale
+//   model.scale.setScalar(settings.scale);
   
-  // Position the model
-  model.position.set(
-    settings.position.x,
-    settings.position.y,
-    settings.position.z
-  );
+//   // Position the model
+//   model.position.set(
+//     settings.position.x,
+//     settings.position.y,
+//     settings.position.z
+//   );
   
-  // Set initially hidden
-  model.visible = false;
+//   // Set initially hidden
+//   model.visible = false;
   
-  // Setup animations
-  model.userData.animations = {};
+//   // Setup animations
+//   model.userData.animations = {};
   
-  if (gltfAsset.animations && gltfAsset.animations.length > 0) {
-    // Create animation mixer
-    const mixer = new THREE.AnimationMixer(model);
-    mixers.push(mixer);
+//   if (gltfAsset.animations && gltfAsset.animations.length > 0) {
+//     // Create animation mixer
+//     const mixer = new THREE.AnimationMixer(model);
+//     mixers.push(mixer);
     
-    // Store mixer reference
-    model.userData.mixer = mixer;
+//     // Store mixer reference
+//     model.userData.mixer = mixer;
     
-    // Process animations
-    const animationMap = settings.animations || {};
+//     // Process animations
+//     const animationMap = settings.animations || {};
     
-    // Find and setup each requested animation
-    Object.keys(animationMap).forEach(key => {
-      const animName = animationMap[key];
-      const anim = gltfAsset.animations.find(a => a.name === animName);
+//     // Find and setup each requested animation
+//     Object.keys(animationMap).forEach(key => {
+//       const animName = animationMap[key];
+//       const anim = gltfAsset.animations.find(a => a.name === animName);
       
-      if (anim) {
-        const action = mixer.clipAction(anim);
-        model.userData.animations[key] = action;
+//       if (anim) {
+//         const action = mixer.clipAction(anim);
+//         model.userData.animations[key] = action;
         
-        // Auto-play idle animation
-        if (key === 'idle') {
-          action.play();
-        }
-      }
-    });
-  }
+//         // Auto-play idle animation
+//         if (key === 'idle') {
+//           action.play();
+//         }
+//       }
+//     });
+//   }
   
-  return model;
-}
+//   return model;
+// }
 
-// Setup characters in the scene
-export function setupCharacters(characterModels, scene) {
-  // Add all characters to the scene
-  Object.values(characterModels).forEach(model => {
-    scene.add(model);
-  });
+// // Setup characters in the scene
+// export function setupCharacters(characterModels, scene) {
+//   // Add all characters to the scene
+//   Object.values(characterModels).forEach(model => {
+//     scene.add(model);
+//   });
   
-  return characterModels;
-}
+//   return characterModels;
+// }
 
-// Update character positions relative to the camera
-export function updateCharacterPositions(characters, camera) {
-  if (!characters || !camera) return;
+// // Update character positions relative to the camera
+// export function updateCharacterPositions(characters, camera) {
+//   if (!characters || !camera) return;
   
-  const camPos = new THREE.Vector3();
-  camera.getWorldPosition(camPos);
+//   const camPos = new THREE.Vector3();
+//   camera.getWorldPosition(camPos);
   
-  const camDir = new THREE.Vector3(0, 0, -1);
-  camDir.applyQuaternion(camera.quaternion);
+//   const camDir = new THREE.Vector3(0, 0, -1);
+//   camDir.applyQuaternion(camera.quaternion);
   
-  // Position wendy in front of the user
-  if (characters.wendy) {
-    const wendyPos = camPos.clone().add(camDir.clone().multiplyScalar(2));
-    wendyPos.y = camPos.y - 0.2; // Slightly below eye level
+//   // Position wendy in front of the user
+//   if (characters.wendy) {
+//     const wendyPos = camPos.clone().add(camDir.clone().multiplyScalar(2));
+//     wendyPos.y = camPos.y - 0.2; // Slightly below eye level
     
-    characters.wendy.position.copy(wendyPos);
-    characters.wendy.lookAt(camPos);
-  }
+//     characters.wendy.position.copy(wendyPos);
+//     characters.wendy.lookAt(camPos);
+//   }
   
-  // Position mendy behind the user
-  if (characters.mendy) {
-    const backDir = camDir.clone().multiplyScalar(-1); // Behind user
-    const mendyPos = camPos.clone().add(backDir.multiplyScalar(2));
-    mendyPos.y = camPos.y - 0.2; // Slightly below eye level
+//   // Position mendy behind the user
+//   if (characters.mendy) {
+//     const backDir = camDir.clone().multiplyScalar(-1); // Behind user
+//     const mendyPos = camPos.clone().add(backDir.multiplyScalar(2));
+//     mendyPos.y = camPos.y - 0.2; // Slightly below eye level
     
-    characters.mendy.position.copy(mendyPos);
-    characters.mendy.lookAt(camPos);
-  }
-}
+//     characters.mendy.position.copy(mendyPos);
+//     characters.mendy.lookAt(camPos);
+//   }
+// }
