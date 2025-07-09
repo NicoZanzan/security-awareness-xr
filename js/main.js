b2e6bd25e02420680e0f2b4a482beafbfdf8882c
Dietrich Kluge <dietrichk@knowbe4.com>
2025-07-09 15:30:58 +0200

    minor corrections

--- js/main.js
+++ js/main.js
@@ -217,7 +217,7 @@ class ARExperience {
                 // Adjust for VR if needed (particularly for Meta Quest)
                 if (isVRSupported && !isARSupported) {
                     //this.adjustForVR();
-                    this.camera.position.set(0, 1.6, 0);
+                    this.camera.position.set(0, -1.6, 0);
                 }
                 
             } else {
